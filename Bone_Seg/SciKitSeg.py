daf79bd5c00aef45b55eacd511167b211492bad7
bigmountainbob <kfran833@gmail.com>
2025-02-11 11:02:28 -0700

    Begin add multiple algorithms

--- Bone_Seg/SciKitSeg.py
+++ Bone_Seg/SciKitSeg.py
@@ -13,26 +13,25 @@ for i in range(len(path_slice)):
     img1 = plt.imread(path_slice[i])
     mask = plt.imread(path_mask[i])
     ar_1 = np.array(img1)
+
+    ## Apply Sobel Filter to images
     # Create histogram of intensity 0 = black
     # fig1 = plt.hist(ar_1)
     # fig2 = plt.imshow(ar_1)
     # Apply Sobel filter with mask
     el_map = ski.filters.sobel(ar_1, mask=mask)
-    can_map = ski.feature.canny(ar_1,mask = mask)
     # Create markers to distinguish from background
     markers = np.zeros_like(ar_1)
     # Edit values to remove background
     markers[ar_1 < 33300] = 1  # Define dark threshold
     markers[ar_1 > 35590] = 2  # Define light threshold
-
     # segmented_bone = ski.segmentation.watershed(el_map,markers) # Lose detail with this
     # segmented_bone = ndi.binary_fill_holes(markers -1)
-
     mask = np.zeros_like(markers)
     mask[markers == 1] = 1
-
     plt.imshow(mask, cmap='grey')
     plt.colorbar()
-    plt.savefig('Segmented_' + str(i) + '.png')
+    plt.savefig('Sobel_' + str(i) + '.png')
     plt.clf()
     # plt.show()
+
