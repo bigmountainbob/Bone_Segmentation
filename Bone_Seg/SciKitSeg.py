4f1cc19d5f7cb59b316f326079d72cc679f25544
bigmountainbob <kfran833@gmail.com>
2025-02-11 11:58:00 -0700

    Begin add multiple algorithms

--- Bone_Seg/SciKitSeg.py
+++ Bone_Seg/SciKitSeg.py
@@ -5,6 +5,8 @@ import numpy as np
 from scipy import ndimage as ndi
 import glob
 
+from skimage.filters import try_all_threshold
+
 path_slice = glob.glob(r'C:\Users\kfran\PycharmProjects\Bone_Segmentation\Bone_Seg\Slices\*.tiff')
 path_mask = glob.glob(r'C:\Users\kfran\PycharmProjects\Bone_Segmentation\Bone_Seg\Masks\*.tiff')
 
@@ -13,25 +15,27 @@ for i in range(len(path_slice)):
     img1 = plt.imread(path_slice[i])
     mask = plt.imread(path_mask[i])
     ar_1 = np.array(img1)
+    mask = mask/255
+    ar_1 = mask*ar_1
 
-    ## Apply Sobel Filter to images
+    ## Greyscale Image Thresholding
+
     # Create histogram of intensity 0 = black
     # fig1 = plt.hist(ar_1)
-    # fig2 = plt.imshow(ar_1)
-    # Apply Sobel filter with mask
-    el_map = ski.filters.sobel(ar_1, mask=mask)
-    # Create markers to distinguish from background
+    # fig = plt.imshow(ar_1)
+    # .show()
+    # plt.clf
+
     markers = np.zeros_like(ar_1)
-    # Edit values to remove background
     markers[ar_1 < 33300] = 1  # Define dark threshold
     markers[ar_1 > 35590] = 2  # Define light threshold
-    # segmented_bone = ski.segmentation.watershed(el_map,markers) # Lose detail with this
-    # segmented_bone = ndi.binary_fill_holes(markers -1)
-    mask = np.zeros_like(markers)
-    mask[markers == 1] = 1
-    plt.imshow(mask, cmap='grey')
+    mask_th = np.zeros_like(markers)
+    mask_th[markers == 1] = 1
+    plt.imshow(mask_th, cmap='grey')
     plt.colorbar()
-    plt.savefig('Sobel_' + str(i) + '.png')
+    plt.savefig('Threshold_' + str(i) + '.png')
     plt.clf()
     # plt.show()
 
+    fig, ax = try_all_threshold(ar_1,figsize=(100,60),verbose=True)
+    plt.show()
